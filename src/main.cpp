f84dae4fb42c2d8fbfff0d94ffd5295ea945b555
Anishka0402 <agmaqueen446@gmail.com>
2026-02-10 09:41:28 +0530

    Initialize OLED display and add basic text output

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,26 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
 void setup() {
 // write your initialization code here
+    Serial.begin(9600);
+    if(!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
+        Serial.println(F("OLED not found"));
+        while(true);
+    }
+    //clear display
+    display.clearDisplay();
+
+    //Set text properties
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+
+    //Display text
+    display.setCursor(0, 0);
+    display.println("Hello, World!");
+    display.println("How are you guys");
+    display.println("Arduino UNO R4");
+
+    //display on Screen
+    display.display();
 }
 
 void loop() {
