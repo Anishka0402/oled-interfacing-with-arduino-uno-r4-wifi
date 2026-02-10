916bf573501dc937614fdd42b9c8f60ab76b29fe
Anishka0402 <agmaqueen446@gmail.com>
2026-02-10 09:21:31 +0530

    Add OLED display support with Adafruit SSD1306 library

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+// OLED I2C address
+#define OLED_ADDR 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
 void setup() {
 // write your initialization code here
 }
